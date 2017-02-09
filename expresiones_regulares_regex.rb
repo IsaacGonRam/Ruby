17d44d96972ea4a7fa0f97955b9aac9fcb7470fe
doberman7 <ivanrubioangeles@gmail.com>
2017-02-09 15:13:00 -0600

    regex fixed

--- expresiones_regulares_regex.rb
+++ expresiones_regulares_regex.rb
@@ -1,3 +1,4 @@
+#format_account = "1234-123-123"
 #---Regresa true si encuentra un numero de cuenta.
   def acount_found(msj)
      /\S\d{4}-\d{3}-\d{3}/ !~ msj
@@ -5,27 +6,29 @@
   end
 #---Regresa un número de cuenta si existe dentro del string y nil  en el caso contrario.
    def return_acount_number(string)
-     /\d{4}-\d{3}-\d{3}/.match(string)
+     /\d{4}-\d{3}-\d{3}/.match(string) ?  string = string.slice(/\d{4}-\d{3}-\d{3}/) : nil;
    end
 #---Regresa un array con los números de cuenta que existen dentro del string y un array vacío en el caso contrario.
    def return_array(string)
      string.scan(/\d{4}-\d{3}-\d{3}/)
    end
-#---Regresa un string donde si existen números de cuenta estos tendran remplazados por "X" los primeros siete numeros. ej. "XXXX-XXX-234"R
-  def change_for_x(string)
-    string.gsub(/\d{4}-\d{3}/,"XXXX-XXX")
-  end
 #---Regresa un string formateado donde cualquier número de diez dígitos seguido o si tiene puntos en vez de guiones lo regresa a su formato original donde usa guiones para dividir los diez dígitos. Si encuentra un numero de menos dígitos no debería remplazarlo
 def formated_string(string)
-    string.gsub(/\d/)
+  if string !~ /\d{10}/ || string !~ /\d{4}\.\d{3}\.\d{3}/
+    four_digits = string.slice!(/\d{4}/)
+    three_digits = string.slice!(/\d{3}/)
+    last_digits = string.slice!(/\d{3}/)
+    four_digits + '-' + three_digits + '-' + last_digits
+  end
 end
-
+#---argumentos
 one_acount_number = "El Cliente con el número de cuenta 1234-123-123 se encuentra en proceso de revisión de sus documentos"
-
 two_acount_number = "Hay que transferir los fondos de la cuenta 1234-123-123 a la cuenta 4321-321-311"
+one_acount_number_with_dots = "El Cliente con el número de cuenta 1234.123.123 se encuentra en proceso de revisión de sus documentos"
+one_acount_number_continued = "El Cliente con el número de cuenta 1234123123 se encuentra en proceso de revisión de sus documentos"
 
-
- acount_found(one_acount_number) == true
- return_acount_number(one_acount_number) #== "1234-123-123"
- return_array(two_acount_number)==["1234-123-123","4321-321-311"]
-p change_for_x(one_acount_number)=="El Cliente con el número de cuenta XXXX-XXX-123 se encuentra en proceso de revisión de sus documentos"
+p acount_found(one_acount_number) == true
+p return_acount_number(one_acount_number) == "1234-123-123"
+p return_array(two_acount_number)==["1234-123-123","4321-321-311"]
+p formated_string(one_acount_number_continued)=="1234-123-123"
+p formated_string(one_acount_number_with_dots)=="1234-123-123"
